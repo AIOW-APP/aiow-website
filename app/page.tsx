831d9e40011adfa8be412e663ca536bad846891e
Handsome <handsomebstrd@gmail.com>
2026-04-19 22:40:13 +0200

    fix: remove SpunkyMorph corner widget (out of V3 design language)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { ProblemV2, MomentV2, FinalCtaV2 } from "@/components/v2/StoryV2";
 import { JourneyV2 } from "@/components/v2/JourneyV2";
 import { SectorsV2 } from "@/components/v2/SectorsV2";
 import { ScanV2 } from "@/components/v2/ScanV2";
-import { SpunkyMorph } from "@/components/v2/SpunkyMorph";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
@@ -17,7 +16,6 @@ export default function Home() {
       <ScanV2 />
       <FinalCtaV2 />
       <Footer />
-      <SpunkyMorph />
     </main>
   );
 }
